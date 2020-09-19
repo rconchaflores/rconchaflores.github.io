7a655be3bf855bb6f4a35dce84e3be18919fbeec
rconchaflores <con18014@byui.edu>
2020-09-19 16:38:24 -0500

    fixed date

--- js/dateModified.js
+++
@@ -1,4 +0,0 @@
-function lastModified() {
-    document.getElementById('dateModified').innerHTML = `Date Modified: ${document.lastModified}`;
-    document.getElementById('year').innerHTML = new Date().getFullYear();
-}
