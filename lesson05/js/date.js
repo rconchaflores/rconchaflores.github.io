dd840408dbeebc0c7599cca87a56b18476a45997
rconchaflores <con18014@byui.edu>
2020-10-15 14:21:03 -0500

    week 05 continues

--- lesson05/js/date.js
+++ lesson05/js/date.js
@@ -1,4 +1,11 @@
 let date = new Date();
-let todaydate = date.getDate();
 
 document.getElementById("update").innerHTML = date.toDateString();
+
+let dayOfWeek = new Date();
+const banner = document.getElementById("alert");
+if (dayOfWeek.getDay() == 5) {
+    banner.style.display = "content";
+} else {
+    banner.style.display = "none";
+}
